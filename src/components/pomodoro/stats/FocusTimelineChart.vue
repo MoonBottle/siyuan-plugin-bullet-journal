66d5fadceb11ec36c71cef285828dc7f3c410b66
wangmin <450527516@qq.com>
2026-03-14 14:11:44 +0800

    style(组件): 调整FocusTimelineChart组件的z-index层级
    添加z-index属性以确保图表元素的正确层叠顺序，避免视觉重叠问题

--- src/components/pomodoro/stats/FocusTimelineChart.vue
+++ src/components/pomodoro/stats/FocusTimelineChart.vue
@@ -298,6 +298,7 @@ function nextWeek() {
   flex-direction: column;
   justify-content: space-between;
   pointer-events: none;
+  z-index: 0;
 }
 
 .grid-line {
@@ -315,6 +316,8 @@ function nextWeek() {
   flex: 1;
   display: flex;
   flex-direction: column;
+  position: relative;
+  z-index: 1;
 }
 
 .hour-cell {
