65fdc88d74f0737c9e895fc89c384e4b1acc1002
wangmin <450527516@qq.com>
2026-04-10 14:46:34 +0800

    feat: create mobile-aware TodoDock entry point

---
+++ src/tabs/TodoDock.vue
@@ -0,0 +1,14 @@
+<template>
+  <MobileTodoDock v-if="isMobile" />
+  <DesktopTodoDock v-else />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { usePlugin } from '@/main';
+import DesktopTodoDock from './DesktopTodoDock.vue';
+import MobileTodoDock from './mobile/MobileTodoDock.vue';
+
+const plugin = usePlugin() as any;
+const isMobile = computed(() => plugin.isMobile);
+</script>
