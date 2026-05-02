4f08d8e1d2755a2f06816392d738b0d2f88e0397
wangmin <450527516@qq.com>
2026-05-02 18:32:51 +0800

    fix(workbench): make overview stats adapt to widget width

--- src/components/workbench/widgets/PomodoroStatsWidget.vue
+++ src/components/workbench/widgets/PomodoroStatsWidget.vue
@@ -79,11 +79,15 @@ const focusDetailRangeOffset = ref(0);
 
 .workbench-widget-pomodoro-stats__content :deep(.chart-card),
 .workbench-widget-pomodoro-stats__content :deep(.heatmap-card),
-.workbench-widget-pomodoro-stats__content :deep(.focus-detail-section),
-.workbench-widget-pomodoro-stats__content :deep(.stats-overview) {
+.workbench-widget-pomodoro-stats__content :deep(.focus-detail-section) {
   display: flex;
   width: 100%;
   height: 100%;
+  min-height: 0;
+}
+
+.workbench-widget-pomodoro-stats__content :deep(.stats-overview) {
+  width: 100%;
   min-height: 0;
 }
 </style>
