58024dc6769bd07f18ba30b0d728a6c59f42e398
wangmin <450527516@qq.com>
2026-05-04 21:11:42 +0800

    文档

--- remotion-video/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { TomatoIcon } from "./TomatoIcon";
-export { ProgressRing } from "./ProgressRing";
-export { AnimatedNumber } from "./AnimatedNumber";
-export { Card } from "./Card";
-export { Background } from "./Background";
