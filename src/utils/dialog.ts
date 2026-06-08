a3f9e9d27cfaadec9fa570be66c4f5ae68e2541a
wangmin <450527516@qq.com>
2026-06-08 09:08:13 +0800

    style(icons): 更新查看日历图标为 calendar-search

--- src/utils/dialog.ts
+++ src/utils/dialog.ts
@@ -541,6 +541,7 @@ export function showPomodoroTimerDialog(preselectedBlockId?: string, initialGrou
     if (mountEl) {
       timerDialogApp = createApp(PomodoroTimerDialog, {
         closeDialog,
+        preselectedBlockId,
         initialGroupId,
       })
       timerDialogApp.mount(mountEl)
